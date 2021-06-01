fbeda27eab8e800bf162a79c70d580d89a08890f
tanvir.rashid <tanvir.rashid@reddotdigitalit.com>
2021-06-02 02:57:44 +0600

    all url from a page scraped

--- pageScraper.js
+++ pageScraper.js
@@ -8,7 +8,7 @@ const scraperObject = {
         // Wait for the required DOM to be rendered
         await page.waitForSelector('.article');
         // Get the link to all the required books
-        let urls = await page.$$eval('#main > div > div.lister.list.detail.sub-list > div > div:nth-child(1) > div.lister-item-content', links => {
+        let urls = await page.$$eval('#main > div > div.lister.list.detail.sub-list > div > div > div.lister-item-content', links => {
             // Make sure the book to be scraped is in stock
             // links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
             // Extract the links from the data
