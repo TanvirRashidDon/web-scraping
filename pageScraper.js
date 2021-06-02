f337a2894f95ae68c2a2d9e29bdabc54e12a49dc
tanvir.rashid <tanvir.rashid@reddotdigitalit.com>
2021-06-02 18:01:07 +0600

    First page urls returned

--- pageScraper.js
+++ pageScraper.js
@@ -7,9 +7,57 @@ const scraperObject = {
         await page.goto(this.url);
 
         let scrapedData = [];
-        let allUrls = [];
 
-        
+        const getAllLinksFromAPage = async (page) => {
+            let urls = await page.$$eval('#main > div > div.lister.list.detail.sub-list > div > div > div.lister-item-content', links => {
+                // add filter if any exist
+                // links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
+                // Extract the links from the data
+                links = links.map(el => el.querySelector('h3 > a').href)
+                return links;
+            });
+
+            return urls;
+        }
+
+        // Get Next page url call
+        const getNextPageUrls = async (page, pageCount) => {
+            let allUrls = []
+            let urls = await getAllLinksFromAPage(page);
+            // console.log(urls);
+          
+            allUrls.push(...urls);
+            
+            if (pageCount <= 0) {
+                return allUrls;
+            } else {
+                for (let i = 0; i < pageCount ; i++) {
+                    let nextButtonExist = false;
+                    try {
+                        const nextButton = await page.$eval('.desc > a', a => a.textContent);
+                        nextButtonExist = true;
+                    } catch (error) {
+                        nextButtonExist = false;
+                        break;
+                    }
+
+                    if (nextButtonExist) {
+                        await page.click('.desc > a');
+                        // return scrapeCurrentPage();
+                        console.log('hellloo');
+                        urls = await getAllLinksFromAPage(page);
+                        console.log('urls' + urls)
+                        allUrls.push(...urls);
+                    
+                    }
+                }
+            }
+
+            return allUrls;
+        }
+
+        let allUrls = await getNextPageUrls(page, 10)
+        console.log(allUrls.length);
 
         async function scrapeCurrentPage(){
 
@@ -17,7 +65,6 @@ const scraperObject = {
             await page.waitForSelector('#main');
 
             // Get the link to all the links
-            availableUrls = getNextPageUrls(page, 10)
 
             let pagePromise = (link) => new Promise(async(resolve, reject) => {
                 let dataObj = {};
@@ -45,7 +92,7 @@ const scraperObject = {
                 await newPage.close();
             })
 
-            for(link in availableUrls){
+            for(link in allUrls){
                 let currentPageData = await pagePromise(urls[link]);
                 scrapedData.push(currentPageData);
                 // console.log(currentPageData);
@@ -54,59 +101,17 @@ const scraperObject = {
             // When all the data on this page is done, click the next button and start the scraping of the next page
             // You are going to check if this button exist first, so you know if there really is a next page.
            
-            console.log(urls)
             await page.close();
 
             return scrapedData;
         }
 
-        const getNextPageUrls = async (page, pageCount) => {
-            
-            let allUrls = []
-            let urls = await getAllLinksFromAPage(page);
-            // console.log(urls);
-          
-            allUrls.push(urls);
-            
-            if (pageCount <= 0) {
-                return allUrls;
-            } else {
-                for (let i = 0; i < pageCount; i++) {
-                    let nextButtonExist = false;
-                    try {
-                        const nextButton = await page.$eval('.desc > a', a => a.textContent);
-                        nextButtonExist = true;
-                    } catch (error) {
-                        nextButtonExist = false;
-                    }
+     
+  
 
-                    if (nextButtonExist) {
-                        await page.click('.desc > a');
-                        // return scrapeCurrentPage();
-                        urls = await getAllLinksFromAPage(page);
-                        allUrls.push(urls);
-                    
-                    }
-                }
-            }
-            return allUrls;
-            
-        }
-        const getAllLinksFromAPage = async (page) => {
-            let urls = await page.$$eval('#main > div > div.lister.list.detail.sub-list > div > div > div.lister-item-content', links => {
-                // add filter if any exist
-                // links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
-                // Extract the links from the data
-                links = links.map(el => el.querySelector('h3 > a').href)
-                return links;
-            });
-
-            return urls;
-        }
-
-        let data = await scrapeCurrentPage();
-        console.log(data);
-        return data;
+        // let data = await scrapeCurrentPage();
+        // console.log(data);
+        // return data;
     }
 }
 
